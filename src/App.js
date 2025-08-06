860490e9d85dbe731c20bd5f32ef722dd86448b1
NoahSayed <noahsayeddb@gmail.com>
2025-08-07 00:34:09 +0530

    full project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+import UseStateHook from "./component/Hooks/useState";
+import UseEffectHook from "./component/Hooks/useEffect";
+import UseReducerHook from "./component/Hooks/useReducer";
+
+const App = () => {
+    return (
+        <>
+            <Resturant />
+            
+        </>
+    );
+};
+
+export default App;
